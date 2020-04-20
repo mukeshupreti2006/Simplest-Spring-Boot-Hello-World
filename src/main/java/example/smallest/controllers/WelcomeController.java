6d101edea27d455842e00c9f9ae0045b1e55faeb
mukeshupreti2006 <mukeshupreti2006@gmail.com>
2020-04-20 11:51:55 +0530

    Update WelcomeController.java

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -1,6 +1,9 @@
 package example.smallest.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,11 +21,10 @@ public class WelcomeController {
 		
 		return "Hello FUCKING World!!!"; //"application/json" mean this is a text not a redirect
 	}
-	
-   @RequestMapping("/hello")
-    public String welcome()
-    {
-        //application code
-        return "Hi This is your first Spring application under Azure pipeline";
+
+    @GetMapping("/get")
+    public @ResponseBody
+    ResponseEntity<String> get() {
+        return new ResponseEntity<String>("GET Response", HttpStatus.OK);
     }
 }
