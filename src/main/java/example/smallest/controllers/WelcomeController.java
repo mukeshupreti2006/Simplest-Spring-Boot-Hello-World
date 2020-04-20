7a4b753ce92b3ac300b8762173bb6e546ee4c8da
mukeshupreti2006 <mukeshupreti2006@gmail.com>
2020-04-20 17:26:29 +0530

    Update WelcomeController.java

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -2,13 +2,14 @@ package example.smallest.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WelcomeController {
 	
 	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
@@ -26,5 +27,11 @@ public class WelcomeController {
     public @ResponseBody
     ResponseEntity<String> get() {
         return new ResponseEntity<String>("GET Response", HttpStatus.OK);
+    }
+	
+   @GetMapping("/me")
+    public @ResponseBody
+    ResponseEntity<String> get() {
+        return new ResponseEntity<String>("Hello Mukesh Upreti", HttpStatus.OK);
     }
 }
